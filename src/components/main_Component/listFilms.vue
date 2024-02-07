c6eb94619cbfc61f00d12a91277d0b42c96e1f5a
Amarildo <you@example.com>
2024-02-07 18:07:00 +0100

    created new components for AppMain

---
+++ src/components/main_Component/listFilms.vue
@@ -0,0 +1,13 @@
+<script>
+import { store } from '../data/store.js';
+import FilmCard from './filmCard.vue';
+export default {
+    name: 'ListFilms',
+    data: () => ({
+        store
+    }),
+    components: {
+        FilmCard
+    }
+}
+</script>
