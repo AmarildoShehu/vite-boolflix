7f1b15bea3c853bab98c7addee03f581abc62d25
Amarildo <you@example.com>
2024-02-08 19:32:01 +0100

    createt serchbar to header

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,8 @@ import AppHeader from './components/AppHeader.vue'
 import AppMain from './components/AppMain.vue'
 
 import { store } from './data/store.js'
+const endpoint = 'https://api.themoviedb.org/3/search';
+const apiKey = '&api_key=c3bd16a79a46d1ae2f5c9428ff80fe'
 
 export default {
     name: 'BoolFlix',
@@ -11,12 +13,25 @@ export default {
     data: () => ({
         store
     }),
+    methods: {
+        fetchShows(input) {
+            if (!input) return;
+            const urlMovies = endpoint + `/movie?query=${input}` + apiKey;
+            const urlTvShows = endpoint + `/tv?query=${input}` + apiKey;
+            axios.get(urlMovies).then(res => {
+                store.listMovies = res.data.results
+            });
+            axios.get(urlTvShows).then(res => {
+                store.listTvShows = res.data.results
+            });
+        }
+    },
 }
 
 </script>
 
 <template>
-    <AppHeader />
+    <AppHeader @search-movies="fetchShows" buttonLabel="Cerca" placeholder="Scrivi qui" />
     <AppMain />
 </template>
 
--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,16 +1,29 @@
 <script >
 export default {
     name: 'AppHeader',
+    data: () => ({
+        inputSearch: '',
+    }), props: {
+        buttonLabel: String,
+        placeholder: String
+    },
+    emits: ['search-movies']
 }
 </script>
 
 <template>
-    <header></header>
+    <header class="container d-flex justify-content-between">
+        <h1>Boolflix</h1>
+        <form @submit.prevent="$emit('search-movies', inputSearch)" id="searchBar">
+            <input type="text" :placeholder="placeholder" v-model.trim="inputSearch">
+            <button>{{ buttonLabel }}</button>
+        </form>
+    </header>
 </template>
 
 <style lang="scss" scoped>
 header {
     height: 100px;
-    background-color: red;
+    border: 1px solid black;
 }
 </style>
